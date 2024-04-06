92c54b413ae761326da545b44e7766aecbd97514
Yasir Aris M <git@yasirdev.my.id>
2024-04-06 21:44:37 +0700

    Update dev.py
    Signed-off-by: Yasir Aris M <git@yasirdev.my.id>

--- misskaty/plugins/dev.py
+++ misskaty/plugins/dev.py
@@ -146,7 +146,7 @@ async def balas(_, ctx: Message) -> "str":
     await ctx.delete_msg()
     await ctx.reply_msg(pesan, reply_to_message_id=ctx.reply_to_message.id)
 
-#@app.on_bot_business_message(group=-97)
+@app.on_bot_business_message(group=-97)
 async def business_handl(self, ctx):
     msg = await self.send_message(ctx.chat.id, "pesan awal", business_connection_id=ctx.business_connection_id)
     await a.edit("Pesan diedit")
